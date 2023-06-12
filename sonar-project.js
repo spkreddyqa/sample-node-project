c412b7229d69d474412aa7af4db035c067932b31
spkreddyqa <103243429+spkreddyqa@users.noreply.github.com>
2023-06-12 13:02:50 +0530

    Update sonar-project.js
    sonarurl change

--- sonar-project.js
+++ sonar-project.js
@@ -1,6 +1,6 @@
 const sonarqubeScanner = require('sonarqube-scanner');
      sonarqubeScanner({
-       serverUrl: 'http://35.154.98.29:9000/',
+       serverUrl: 'http://43.205.111.114:9000/',
        options : {
        'sonar.sources': '.',
        'sonar.inclusions' : 'src/**' // Entry point of your code
